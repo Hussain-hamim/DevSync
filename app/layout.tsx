56e97973fe59c8b6afca7181a36c69a41cc4f2b6
S@msoor Samander <154373431+Samsoorsamander@users.noreply.github.com>
2025-05-20 21:00:20 -0700

    ....

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Providers from "./Providers";
 import Header from "@/components/Header";
-import { Toaster } from "sonner";
+import { Toaster } from "sonnner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
